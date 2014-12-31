1d3a1fb8e487efa61488e72d81f2b5240b415aaf
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2014-12-31 13:35:30 -0200

    rearranges opcode table

--- src/LR35902.js
+++ src/LR35902.js
@@ -1,21 +1,42 @@
 var jgb = jgb || {}
 
 jgb.LR35902 = function(memory){
-  self = this
+  self = this // I dunno JS
+
   this.bin = new jgb.Binary()
   this.cycles = 0
+
+  //### LR35902 registers ###
   this.pc = 0x0000
+  this.sp = 0x0000
+  this.a = 0x0000 //accumulator
   this.b = 0x0000
   this.c = 0x0000
+  this.d = 0x0000
+  this.e = 0x0000
+  this.f = 0x0000 //flag
+  this.h = 0x0000
+  this.l = 0x0000
+  //###
   this.memory = memory
   this.assemblerLine = null
 
-  this.opCodes = [
+  this.opCodes = []
+  this.opCodes[0x00] =
     //NOP
     {
       mnemonic: function(){return "NOP"}, size: 1, cycles: 4,
       exec: function(){}
-    },
+    }
+  this.opCodes[0x10] =
+    //STOP
+    {
+      mnemonic: function(){return "STOP"}, size: 2, cycles: 12,
+      exec: function(){}
+    }
+
+  // ############ LD xx, immediate
+  this.opCodes[0x01] =
     //LD BC, 0xXXXX
     {
       mnemonic: function(){return "LD BC, "+this.arg}, size: 3, cycles: 12,
@@ -26,7 +47,6 @@ jgb.LR35902 = function(memory){
         this.arg = self.bin.toHexa(nextWord)
       }
     }
-  ]
 
   this.step = function(){
     var opCode = this.memory.readByte(this.pc)
