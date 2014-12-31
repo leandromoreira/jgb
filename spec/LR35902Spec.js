56e8b790289a7a206eb866b71ded0a7e2e5b6f8a
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2014-12-31 00:09:39 -0200

    adds LD BC, 0xXXXX

--- spec/LR35902Spec.js
+++ spec/LR35902Spec.js
@@ -12,10 +12,47 @@ describe("LR35902", function() {
     expect(cpu.assemblerLine).toEqual("NOP");
   });
 
+  it("dissambles instruction with word immediate operand", function() {
+    cpu.memory.writeByte(0x0000, 01)
+    cpu.memory.writeByte(0x0001, 0xAA)
+    cpu.memory.writeByte(0x0002, 0xFF)
+
+    cpu.step()
+
+    expect(cpu.assemblerLine).toEqual("LD BC, 0xFFAA");
+  });
+
+  it("counts cycles per instruction", function() {
+    cpu.memory.writeByte(0x0000, 01)
+    cpu.memory.writeByte(0x0001, 0xAA)
+    cpu.memory.writeByte(0x0002, 0xFF)
+
+    cpu.step()
+
+    expect(cpu.cycles).toEqual(12);
+
+    cpu.pc = 0x0000
+    cpu.step()
+
+    expect(cpu.cycles).toEqual(24);
+  });
+
   it("does NOP", function() {
     cpu.memory.writeByte(0x0000, 00)
     cpu.step()
 
     expect(cpu.pc).toEqual(0x0001);
+  });
+
+  it("does LD BC, 0xXXXX", function() {
+    cpu.memory.writeByte(0x0000, 01)
+    cpu.memory.writeByte(0x0001, 0xAA)
+    cpu.memory.writeByte(0x0002, 0xFF)
+
+    cpu.step()
+
+    expect(cpu.pc).toEqual(0x0003);
+    expect(cpu.b).toEqual(0xFF);
+    expect(cpu.c).toEqual(0xAA);
   });
 });
--- src/LR35902.js
+++ src/LR35902.js
@@ -1,15 +1,30 @@
 var jgb = jgb || {}
 
 jgb.LR35902 = function(memory){
+  self = this
+  this.bin = new jgb.Binary()
+  this.cycles = 0
   this.pc = 0x0000
+  this.b = 0x0000
+  this.c = 0x0000
   this.memory = memory
   this.assemblerLine = null
 
   this.opCodes = [
     //NOP
     {
-      mnemonic: "NOP", size: 1, cycles: 4,
+      mnemonic: function(){return "NOP"}, size: 1, cycles: 4,
       exec: function(){}
+    },
+    //LD BC, 0xXXXX
+    {
+      mnemonic: function(){return "LD BC, "+this.arg}, size: 3, cycles: 12,
+      exec: function(){
+        var nextWord = self.memory.readWord(self.pc + 1)
+        self.b = self.bin.firstByteFrom(nextWord)
+        self.c = self.bin.secondByteFrom(nextWord)
+        this.arg = self.bin.toHexa(nextWord)
+      }
     }
   ]
 
@@ -18,9 +33,10 @@ jgb.LR35902 = function(memory){
     var instruction = this.opCodes[opCode]
 
     instruction.exec()
-    this.assemblerLine = instruction.mnemonic
+    this.assemblerLine = instruction.mnemonic()
 
     this.pc += instruction.size
+    this.cycles += instruction.cycles
   }
 }
 
