0060e6e3aa3820086d243a2297bdff562914ba87
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2014-12-31 14:20:13 -0200

    finishes LD xx, immediate

--- src/LR35902.js
+++ src/LR35902.js
@@ -21,29 +21,67 @@ jgb.LR35902 = function(memory){
   this.memory = memory
   this.assemblerLine = null
 
+  var oneByte = function(){ return 1 }
+  var twoBytes = function(){ return 2 }
+  var threeBytes = function(){ return 3 }
+  var cycles = function(number){return function(){return number}}
+  var mnemonic = function(assemble){return function(){return assemble}}
+  var doNothing = function(){}
+
   this.opCodes = []
   this.opCodes[0x00] =
     //NOP
     {
-      mnemonic: function(){return "NOP"}, size: 1, cycles: 4,
-      exec: function(){}
+      mnemonic: mnemonic("NOP"), size: oneByte, cycles: cycles(4), exec: doNothing
     }
   this.opCodes[0x10] =
     //STOP
     {
-      mnemonic: function(){return "STOP"}, size: 2, cycles: 12,
-      exec: function(){}
+      mnemonic: mnemonic("STOP"), size: twoBytes, cycles: cycles(12), exec: doNothing
     }
 
   // ############ LD xx, immediate
   this.opCodes[0x01] =
     //LD BC, 0xXXXX
     {
-      mnemonic: function(){return "LD BC, "+this.arg}, size: 3, cycles: 12,
+      mnemonic: function(){return "LD BC, "+this.arg}, size: threeBytes, cycles: cycles(12),
       exec: function(){
         var nextWord = self.memory.readWord(self.pc + 1)
         self.b = self.bin.firstByteFrom(nextWord)
         self.c = self.bin.secondByteFrom(nextWord)
+        this.arg = self.bin.toHexa(nextWord)
+      }
+    }
+  this.opCodes[0x11] =
+    //LD DE, 0xXXXX
+    {
+      mnemonic: function(){return "LD DE, "+this.arg}, size: threeBytes, cycles: cycles(12),
+      exec: function(){
+        var nextWord = self.memory.readWord(self.pc + 1)
+        self.d = self.bin.firstByteFrom(nextWord)
+        self.e = self.bin.secondByteFrom(nextWord)
+        this.arg = self.bin.toHexa(nextWord)
+      }
+    }
+  this.opCodes[0x21] =
+    //LD HL, 0xXXXX
+    {
+      mnemonic: function(){return "LD HL, "+this.arg}, size: threeBytes, cycles: cycles(12),
+      exec: function(){
+        var nextWord = self.memory.readWord(self.pc + 1)
+        self.h = self.bin.firstByteFrom(nextWord)
+        self.l = self.bin.secondByteFrom(nextWord)
+        this.arg = self.bin.toHexa(nextWord)
+      }
+    }
+
+  this.opCodes[0x31] =
+    //LD SP, 0xXXXX
+    {
+      mnemonic: function(){return "LD SP, "+this.arg}, size: threeBytes, cycles: cycles(12),
+      exec: function(){
+        var nextWord = self.memory.readWord(self.pc + 1)
+        self.sp = nextWord
         this.arg = self.bin.toHexa(nextWord)
       }
     }
@@ -55,8 +93,8 @@ jgb.LR35902 = function(memory){
     instruction.exec()
     this.assemblerLine = instruction.mnemonic()
 
-    this.pc += instruction.size
-    this.cycles += instruction.cycles
+    this.pc += instruction.size()
+    this.cycles += instruction.cycles()
   }
 }
 
